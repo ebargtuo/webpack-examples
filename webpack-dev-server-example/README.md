3201cc89132f57dd89e993b4bac3bb1fb7b8aea5
ebargtuo <ebargtuo@gmx.it>
2015-04-22 22:22:34 +0200

    Add webpack-dev-server example

---
+++ webpack-dev-server-example/README.md
@@ -0,0 +1,9 @@
+# Basic Webpack Dev Server Example
+
+A simple webpack example with a the
+[webpack-dev-server](https://github.com/webpack/webpack-dev-server).
+
+## Build Dependencies
+
+- webpack
+- webpack-dev-server
---
+++ webpack-dev-server-example/src/js/headingComponent.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var render = function() {
+    var title = document.createElement("h1");
+    var body = document.getElementsByTagName("body")[0];
+    title.appendChild(document.createTextNode("Hello webpack dev server!"));
+    body.appendChild(title);
+};
+
+module.exports = {render: render};
